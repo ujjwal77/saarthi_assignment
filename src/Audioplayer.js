4d9688ab814b433d3bcb39ad06411cef36aa4457
Ujjwal Kumar <ujjwalkumar.min18@itbhu.ac.in>
2021-11-04 06:23:56 +0000

    update

--- src/Audioplayer.js
+++ src/Audioplayer.js
@@ -4,11 +4,25 @@ import ReactDOM from "react-dom"
 import WaveSurfer from "wavesurfer.js"
 import { WaveformContianer, Wave, PlayButton } from "./Waveformstyled"
 import { createContext } from "react"
+// import { useStateValue } from "./StateProvider";
 import "./Audioplayer.css"
+import play from "./Assets/play.png"
+import volume from "./Assets/volume.png"
+import mute from "./Assets/mute.png"
+import pause from "./Assets/pause.png"
+import stop from "./Assets/stop.png"
+
+// function Audioplayer(props){
+//      console.log(props.files)
+//     return <img src={props.files} width={400} height={300}></img>;
+// }
+// export default Audioplayer
 
 class Audioplayer extends Component {
+
     state = {
         Playing: false,
+        vol:false,
     }
 
     componentDidMount() {
@@ -19,12 +33,14 @@ class Audioplayer extends Component {
             cursorWidth: 1,
             container: "#waveform",
             backend: "WebAudio",
-            height: 80,
+            height: 150,
             progressColor: "#2D5BFF",
             responsive: true,
             waveColor: "#EFEFEF",
             cursorColor: "transparent",
+            
         })
+        
 
         this.waveform.load(track)
     }
@@ -33,16 +49,47 @@ class Audioplayer extends Component {
         this.setState({ playing: !this.state.playing })
         this.waveform.playPause()
     }
+    handleReplay = () => {
+        this.setState({ playing: false})
+        this.waveform.stop()
+    }
+
+    handleVolume = () => {
+        this.setState({ vol: !this.state.vol })
+        this.waveform.toggleMute()
+    }
+
+    handleVol = () => {
+        this.waveform.setVolume(0.5);
+        // wavesurfer.current.play();
+      }
+
+    
 
     render() {
         return (
             <div className="Audioplayer">
                 <WaveformContianer>
+                    <div className="sound">
+                        <div className="soundaudio">
+                            <Wave id="waveform" />
+                            <audio id="track" src={song} />
+                        </div>
+
+                    <div className="soundcheck">
                     <PlayButton onClick={this.handlePlay}>
-                        {!this.state.playing ? "Play" : "Pause"}
+                        {!this.state.playing ? <img src={play} height={40} /> : <img src={pause} height={50} />}
                     </PlayButton>
-                    <Wave id="waveform" />
-                    <audio id="track" src={song} />
+                    <PlayButton onClick={this.handleReplay}>
+                        <img src={stop} height={50} />
+                    </PlayButton>
+                    <PlayButton onClick={this.handleVolume}>
+                        {!this.state.vol ? <img src={volume} height={50} /> : <img src={mute} height={50} />}
+                    </PlayButton>
+                    
+                    </div>
+                    </div>
+                    
                 </WaveformContianer>
             </div>
         )
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,6 @@
 import react, { Component } from "react"
+import React from "react";
+// import FileBase from 'react-file-base64';
 import {
     BrowserRouter as Router,
     Route,
@@ -8,17 +10,37 @@ import {
 } from "react-router-dom"
 import Audioplayer from "./Audioplayer"
 import "./Home.css"
-import FancyButton from "./fancybtn"
 
 function Home() {
+    const [file, setFile] = React.useState("");
+
+    function upload(event){
+        setFile(event.target.files[0]);
+    };
+
+    // function handleSubmit(){
+    //     console.log('ukk')
+    //     return(
+    //         <div>
+    //             {file && <Audioplayer files={file}/>}
+    //         </div>
+    //     )
+    // };
+
     return (
         <div className="Home">
-            <Link to="./Audioplayer">
-                {
-                    //}<button id="fancy-button" type="button"> Upload </button>
+                <div className="upload"> 
+                <input type="file" onChange={upload} accept="audio/*" name="Audio"/>
+                {/* <div><FileBase type="file" multiple={false} onDone={({ base64 }) => setFile({selectedFile: base64 })} /></div> */}
+                <br/>
+                {/* {file && <button onClick={handleSubmit}>Trim</button>} */}
+                {file && <Link to="./Audioplayer">
+                <button id="fancy-button">Trim</button>
+                </Link>
                 }
-                <FancyButton id="fancy-button">Upload</FancyButton>
-            </Link>
+                
+                </div>
+                
         </div>
     )
 }
